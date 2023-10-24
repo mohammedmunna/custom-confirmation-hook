f7927e569f593f1a038c83aa4ab873d32565b173
mohammedmunna <mohammed.munna@outlook.com>
2023-10-24 08:28:37 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import "./styles.css";
+import useConfirmation from "./Confirmation";
+
+const DeleteWithUseConfirm = () => {
+  const [getConfirmation, ConfirmationDialog] = useConfirmation();
+
+  const onDelete = async () => {
+    const status = await getConfirmation({
+      offer: { name: "MyOffer" },
+      entity: "product-category",
+      item: { name: "MyCategory" },
+      buttons: [
+        { label: "create", value: "create" },
+        { label: "map", value: "map" },
+        { label: "skip", value: "skip" }
+      ]
+    });
+
+    console.log({ status });
+
+    if (status === "map") {
+      console.log({ status });
+    } else {
+      //  console.log({ status });
+    }
+  };
+
+  return (
+    <>
+      <button onClick={onDelete}>Click Me</button>
+      <ConfirmationDialog />
+    </>
+  );
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Custom Confirmation</h1>
+      <h2>Async/Await Custom Confirmation React Hoook</h2>
+      <DeleteWithUseConfirm />
+    </div>
+  );
+}
---
+++ src/Confirmation.js
@@ -0,0 +1,211 @@
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import _ from "lodash";
+import {
+  Dialog,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  Button,
+  Typography
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    textTransform: "capitalize",
+    margin: "0 10px",
+    borderRadius: "16px",
+    minWidth: "6rem"
+  }
+}));
+
+// useConfirmation hook
+export default function useConfirmation() {
+  const classes = useStyles();
+  const [openDefault, setOpenDefault] = useState(false);
+  const [openMap, setOpenMap] = useState(false);
+  const [resolver, setResolver] = useState();
+  const [dialogState, setDialogState] = useState({
+    offer: {},
+    entity: "",
+    item: {}
+  });
+  const [buttons, setButtons] = useState([
+    { label: "create", value: "create" },
+    { label: "map", value: "map" },
+    { label: "skip", value: "skip" }
+  ]);
+
+  // Create a new Promise and capture its resolve function in the resolver variable
+  const createPromise = () => {
+    let resolver;
+    const promise = new Promise((resolve, reject) => {
+      resolver = resolve;
+    });
+    return [promise, resolver];
+  };
+
+  console.log({ dialogState });
+
+  const getConfirmation = async ({
+    offer = {},
+    entity = "",
+    item = {},
+    buttons = [
+      { label: "create", value: "create" },
+      { label: "skip", value: "skip" }
+    ]
+  }) => {
+    setDialogState({ offer, entity, item });
+    setButtons(buttons);
+    setOpenDefault(true);
+    const [promise, resolve] = await createPromise();
+    console.log(promise, resolve);
+    setResolver(() => resolve);
+    return promise;
+  };
+
+  const joinArrayWithOr = (arr, key) => {
+    return arr
+      .map((item) => item[key])
+      .join(", ")
+      .replace(/,([^,]*)$/, " or $1");
+  };
+
+  const handleClick = async (status) => {
+    setDialogState((prev) => ({ ...prev, status: status }));
+    if (status === "map") {
+      setOpenDefault(false);
+      setOpenMap(true);
+    } else {
+      setOpenDefault(false);
+      resolver(status);
+    }
+  };
+
+  const handleConfirm = async (status) => {
+    setDialogState((prev) => ({ ...prev, status: status }));
+    setOpenMap(false);
+    //setOpenDefault(false);
+    resolver(status);
+  };
+
+  const handleBack = () => {
+    setDialogState((prev) => ({ ...prev, status: "default" }));
+    setOpenMap(false);
+    setOpenDefault(true);
+  };
+
+  const MapContent = () => (
+    <div>
+      <Dialog open={openMap} fullWidth maxWidth={"md"}>
+        <DialogTitle id="map-confirmation-dialog-title">
+          {`${_.startCase(dialogState?.entity || "Item")}`}
+        </DialogTitle>
+        <DialogContent style={{ padding: "1rem 1.5rem" }}>
+          <DialogContentText style={{ marginBottom: "0.1rem" }}>
+            TABLE
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions
+          style={{
+            padding: "0.8rem"
+          }}
+        >
+          <Button
+            onClick={() => handleBack()}
+            variant="contained"
+            color="primary"
+            className={classes.button}
+          >
+            Back
+          </Button>
+          <Button
+            onClick={() => handleConfirm("confirm")}
+            variant="contained"
+            color="primary"
+            className={classes.button}
+          >
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+
+  const DefaultContent = () => (
+    <div>
+      <Dialog open={openDefault} fullWidth maxWidth={"sm"}>
+        <DialogTitle id="default-confirmation-dialog-title">
+          {`${_.startCase(dialogState?.entity || "Item")} Not Found`}
+        </DialogTitle>
+        <DialogContent style={{ padding: "1rem 1.5rem" }}>
+          <DialogContentText style={{ marginBottom: "0.1rem" }}>
+            <Typography
+              gutterBottom
+            >{`Below entity of offer ${dialogState?.offer?.name} is not found.`}</Typography>
+            <Typography
+              gutterBottom
+              style={{ fontWeight: "bold" }}
+            >{`${_.startCase(dialogState?.entity)} : ${
+              dialogState?.item?.name
+            }`}</Typography>
+            <Typography>
+              {`Would you like to ${joinArrayWithOr(buttons, "label")}?`}
+            </Typography>
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions
+          style={{
+            padding: "1.5rem",
+            justifyContent: "center"
+          }}
+        >
+          {buttons.map((b) => (
+            <Button
+              key={b.value}
+              onClick={() => handleClick(b.value)}
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              {b.label}
+            </Button>
+          ))}
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+
+  const ConfirmationDialog = () => {
+    /* 
+    let content = null;
+    switch (dialogState.status) {
+      case "map":
+        content = <MapContent />;
+        break;
+      default:
+        content = <DefaultContent />;
+    }
+    */
+
+    return (
+      <div>
+        {/* 
+        <Dialog
+          open={open}
+          fullWidth
+          maxWidth={dialogState.status === "map" ? "md" : "sm"}
+        >
+          {content}
+        </Dialog>
+        */}
+        <DefaultContent />
+        <MapContent />
+      </div>
+    );
+  };
+
+  return [getConfirmation, ConfirmationDialog];
+}
